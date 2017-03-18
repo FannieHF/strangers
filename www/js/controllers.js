4222e7b6c291ce6f52f1cfa185cb49407d66b310
Vente1 <michaelmarshall208@hotmail.com>
2017-03-19 06:29:00 +1100

    DO it

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,11 @@
-angular.module('app.controllers', [ 'cordovaGeolocationModule'])
+angular.module('app.controllers', ['cordovaGeolocationModule'])
 
      
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-	
+
 
 }])
    
@@ -13,7 +13,7 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-	
+
 
 }])
    
@@ -23,7 +23,6 @@ function ($scope, $stateParams) {
 function ($scope, $stateParams) {
 
 
-
 }])
    
 .controller('newChallengeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
@@ -31,24 +30,23 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+    // // Get user id
+    // var user = firebase.auth().currentUser;
+    $scope.uid = '4D5cqPZOkBROEXxOTAjEOxhkr6C3';
 
-	// // Get user id
-	// var user = firebase.auth().currentUser;
-	$scope.uid = '4D5cqPZOkBROEXxOTAjEOxhkr6C3';
-
-	// if (user != null) {
-	//   $scope.uid = user;
-	// }
+    // if (user != null) {
+    //   $scope.uid = user;
+    // }
 
 
-	// Get a reference to the database service
-	var database = firebase.database();
+    // Get a reference to the database service
+    var database = firebase.database();
 
-	// Get user profile info
-	var Points = firebase.database().ref('users/' + $scope.uid + '/Points');
-	Points.on('value', function(snapshot) {
-		$scope.Points = snapshot.val();
-	});
+    // Get user profile info
+    var Points = firebase.database().ref('users/' + $scope.uid + '/Points');
+    Points.on('value', function(snapshot) {
+        $scope.Points = snapshot.val();
+    });
 
 }])
    
@@ -71,44 +69,42 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
-
 }])
    
 .controller('missionCompleteCtrl', ['$scope','$timeout','$window','$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $timeout, $window, $stateParams) {//10 seconds delay
-	
-	var scoreObtained = 800; // will be some function later; 
-
-	$scope.Score = scoreObtained;
-
-	var uid = '4D5cqPZOkBROEXxOTAjEOxhkr6C3'; //michael
-
-	var prevScore = 50;
-
-	var prevScoreLoc = firebase.database().ref('users/' + uid + '/Points');
-	prevScoreLoc.once('value', function(snapshot) {
-		prevScore = snapshot.val();
-		var newScore = prevScore + scoreObtained;
-		var updates = {};
-	    updates['users/' + uid + '/Points'] = newScore;
-
-	    firebase.database().ref().update(updates);
-	});
+function ($scope,$timeout, $window,$stateParams) {
     
-	$timeout( function(){
+    var scoreObtained = 800; // will be some function later; 
+
+    $scope.Score = scoreObtained;
+
+    var uid = '4D5cqPZOkBROEXxOTAjEOxhkr6C3'; //michael
+
+    var prevScore = 50;
+
+    var prevScoreLoc = firebase.database().ref('users/' + uid + '/Points');
+    prevScoreLoc.once('value', function(snapshot) {
+        prevScore = snapshot.val();
+        var newScore = prevScore + scoreObtained;
+        var updates = {};
+        updates['users/' + uid + '/Points'] = newScore;
+
+        firebase.database().ref().update(updates);
+    });
+    
+    //8 seconds delay
+    $timeout( function(){
         $window.location.href = '#/friendRequest';
     }, 8000)
-
 }])
 
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-
 function ($scope, $stateParams) {
-	
+
 }])
 
 .controller('homeCtrl', function ($scope,$stateParams, cordovaGeolocationService) {
@@ -118,6 +114,7 @@ function ($scope, $stateParams) {
     var successHandler = function (position) {
         $scope.currentPosition = position;
         var latitude = position.coords.latitude;
+        $scope.latitude = latitude;
         var longitude = position.coords.longitude;
         var timestamp = position.timestamp;
     };
@@ -127,47 +124,45 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope,$stateParams) {
-<<<<<<< HEAD
-	
+   
+    // // Get user id
+    // var user = firebase.auth().currentUser;
+    $scope.uid = '4D5cqPZOkBROEXxOTAjEOxhkr6C3'; //michael
 
-	// // Get user id
-	// var user = firebase.auth().currentUser;
-	$scope.uid = '4D5cqPZOkBROEXxOTAjEOxhkr6C3'; //michael
+    // Get a reference to the database service
+    var database = firebase.database();
 
-	// Get a reference to the database service
-	var database = firebase.database();
+    // Get user profile info
+    var FirstName = firebase.database().ref('users/' + $scope.uid + '/FirstName');
+    FirstName.on('value', function(snapshot) {
+        $scope.FirstName = snapshot.val();
+    });
 
-	// Get user profile info
-	var FirstName = firebase.database().ref('users/' + $scope.uid + '/FirstName');
-	FirstName.on('value', function(snapshot) {
-		$scope.FirstName = snapshot.val();
-	});
+    var LastName = firebase.database().ref('users/' + $scope.uid + '/LastName');
+    LastName.on('value', function(snapshot) {
+        $scope.LastName = snapshot.val();
+    });
 
-	var LastName = firebase.database().ref('users/' + $scope.uid + '/LastName');
-	LastName.on('value', function(snapshot) {
-		$scope.LastName = snapshot.val();
-	});
+    var Username = firebase.database().ref('users/' + $scope.uid + '/Username');
+    Username.on('value', function(snapshot) {
+        $scope.Username = snapshot.val();
+    });
 
-	var Username = firebase.database().ref('users/' + $scope.uid + '/Username');
-	Username.on('value', function(snapshot) {
-		$scope.Username = snapshot.val();
-	});
+    var ProfilePicture = firebase.database().ref('users/' + $scope.uid + '/ProfilePicture');
+    ProfilePicture.on('value', function(snapshot) {
+        $scope.ProfilePicture = snapshot.val();
+    });
 
-	var ProfilePicture = firebase.database().ref('users/' + $scope.uid + '/ProfilePicture');
-	ProfilePicture.on('value', function(snapshot) {
-		$scope.ProfilePicture = snapshot.val();
-	});
+    var Points = firebase.database().ref('users/' + $scope.uid + '/Points');
+    Points.on('value', function(snapshot) {
+        $scope.Points = snapshot.val();
+    });
 
-	var Points = firebase.database().ref('users/' + $scope.uid + '/Points');
-	Points.on('value', function(snapshot) {
-		$scope.Points = snapshot.val();
-	});
-
-	var Rank = firebase.database().ref('users/' + $scope.uid + '/Rank');
-	Rank.on('value', function(snapshot) {
-		$scope.Rank = snapshot.val();
-	});
-
+    var Rank = firebase.database().ref('users/' + $scope.uid + '/Rank');
+    Rank.on('value', function(snapshot) {
+        $scope.Rank = snapshot.val();
+    });
+    
 }])
 
 
@@ -175,7 +170,7 @@ function ($scope,$stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-	
+
 }])
    
 .controller('resourceCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
